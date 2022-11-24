8cbf0b175b5ae44ad8a26c7b2d5a0ad09ce06d48
frontWeb-dev <frontend_web@kakao.com>
2022-11-25 02:03:10 +0900

    feat: 날씨 이미지 추가

---
+++ mocks/icons.js
@@ -0,0 +1,9 @@
+export const icons = {
+  Clear: 'https://cdn-icons-png.flaticon.com/512/1163/1163662.png',
+  Clouds: 'https://cdn-icons-png.flaticon.com/512/1163/1163624.png',
+  Rain: 'https://cdn-icons-png.flaticon.com/512/1163/1163626.png',
+  Atmosphere: 'https://cdn-icons-png.flaticon.com/512/1163/1163634.png',
+  Snow: 'https://cdn-icons-png.flaticon.com/512/3753/3753718.png',
+  Drizzle: 'https://cdn-icons-png.flaticon.com/512/4388/4388798.png',
+  Thunderstorm: 'https://cdn-icons-png.flaticon.com/512/1163/1163636.png',
+};
